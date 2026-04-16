19d7f35eeb1c11af84f55115e49f5d7e3d158eba
Callebe Severo <callebesev@gmail.com>
2026-04-16 11:03:54 -0300

    rebase error, so commit to [calculate score]

--- src/js/ranking.js
+++ src/js/ranking.js
@@ -1,10 +1,29 @@
 import { getLastRanking, getLastRankingDate } from "./dbRequest";
+import { calculateScore } from "./score";
+
+export async function formatRankingToDB() {
+    const cards = Array.from(
+        document.querySelectorAll(".people-rating-card")
+    );
+
+    const scores = await Promise.all(cards.map(async (card) => {
+        const peopleName = card.dataset.name;
+        const peopleScore = await calculateScore(card);
+
+        return { "name": peopleName, "score": peopleScore };
+    }));
+    
+    // ordenate ranking by score
+    scores.sort((a, b) => b.score - a.score);
+    
+    return scores;
+};
 
 export function formatRankingToText() {
     const date = getLastRankingDate();
     const ranking = getLastRanking();
 
-    const textRanking = ranking.map(people => {
+    const textRanking = ranking.map((people) => {
         if (people === 1) {        // gold medal 🥇
             return `🥇 ${people["name"]} - ${people["score"]} pontos\n`
         } else if (people === 2) { // silver medal 🥈
