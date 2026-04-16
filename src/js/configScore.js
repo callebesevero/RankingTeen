321bd52735f4f267bd735c0f1c41f0ccb25f496b
Callebe Severo <callebesev@gmail.com>
2026-04-16 10:31:03 -0300

    calculate score

---
+++ src/js/configScore.js
@@ -0,0 +1,11 @@
+export const scoringMethods = {
+    "punctuality": 3,
+    "bible": 2,
+    "comment": 5,
+    "pray": 2,
+    "read": 2,
+    "study": 2,
+    "visitor": 10,
+    "talk": -5,   // Penalty
+    "smartphone": -5 // Penalty
+};
