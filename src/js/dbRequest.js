c0ec294ffaf9d3025480c0f9be842f312c0c18b6
Callebe Severo <callebesev@gmail.com>
2026-09-19 22:35:36 +0000

    estrutura db (fixed 2)

--- src/js/dbRequest.js
+++ src/js/dbRequest.js
@@ -13,7 +13,7 @@ export async function getLastRanking() {
 
 export async function getLastRankedPeople() {
     const object = await getLastDBObject();
-    const ranking = object?.item.ranking;
+    const ranking = object?.item?.ranking;
 
     if (!ranking) {
         return null;
