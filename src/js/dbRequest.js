1da1ff7c1e9812200fe21bb617dcee0ff78f3eb2
Callebe Severo <callebesev@gmail.com>
2026-09-19 22:20:58 +0000

    add driblaBesta correct 1

--- src/js/dbRequest.js
+++ src/js/dbRequest.js
@@ -79,7 +79,7 @@ export async function addPeopleToDB(
 export async function addToDB(
     item
 ) {
-    await set(ref(db, "/"), item), {
+    await set(ref(db, "/"), item, {
         driblaBesta: "subversivos"
-    };
+    });
 };
